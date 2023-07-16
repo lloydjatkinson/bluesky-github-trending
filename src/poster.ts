492cdf7e4302b7c0fc77379c363d894aa7489668
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2023-07-16 01:35:53 +0000

    working around atproto library issues

--- src/poster.ts
+++ src/poster.ts
@@ -1,4 +1,4 @@
-import { BskyAgent } from 'npm:@atproto/api@0.4.0';
+import bsky, { BskyAgent } from 'npm:@atproto/api@0.4.0';
 import { RepositoryStatistics } from './types.ts';
 import { Logger } from 'https://deno.land/std@0.193.0/log/mod.ts';
 import { filterRepositories } from './filter.ts';
@@ -11,10 +11,9 @@ type PostingResult =
     | { success: true }
     | { success: false };
 
-export const postStatistics = (
+export const postStatistics = async (
     {
         logger,
-        // deno-lint-ignore no-unused-vars
         agentService,
         repositories,
     }: {
@@ -31,7 +30,67 @@ export const postStatistics = (
     logger.info(
         `Creating post content based on ${filtered.length} repositories`,
     );
-    console.log(filtered);
+
+    // await agentService.post();
+
+    // creating richtext
+    const { RichText, AppBskyFeedPost } = bsky;
+    // const rt = new RichText({ text: 'hello' });
+    // const postRecord = {
+    //     $type: 'app.bsky.feed.post',
+    //     text: rt.text,
+    //     facets: rt.facets,
+    //     createdAt: new Date().toISOString(),
+    // };
+
+    const rt = new RichText({
+        text:
+            'Hello @lja.bsky.social, check out this link: https://example.com',
+    });
+    await rt.detectFacets(agentService); // automatically detects mentions and links
+    const postRecord = {
+        $type: 'app.bsky.feed.post',
+        text: rt.text,
+        facets: rt.facets,
+        createdAt: new Date().toISOString(),
+    };
+
+    // if (AppBskyFeedPost.isRecord(postRecord)) {
+    //     // typescript now recognizes `post` as a AppBskyFeedPost.Record
+    //     // however -- we still need to validate it
+    //     const res = AppBskyFeedPost.validateRecord(postRecord);
+    //     if (res.success) {
+    //         // a valid record
+    //     } else {
+    //         // something is wrong
+    //         console.log(res.error);
+    //     }
+    // }
+
+    // await agentService.post(postRecord);
+
+    // // rendering as markdown
+    // let markdown = '';
+    // for (const segment of rt.segments()) {
+    //     if (segment.isLink()) {
+    //         markdown += `[${segment.text}](${segment.link?.uri})`;
+    //     } else if (segment.isMention()) {
+    //         markdown +=
+    //             `[${segment.text}](https://my-bsky-app.com/user/${segment.mention?.did})`;
+    //     } else {
+    //         markdown += segment.text;
+    //     }
+    // }
+
+    // // calculating string lengths
+    // const rt2 = new RichText({ text: 'Hello' });
+    // console.log(rt2.length); // => 5
+    // console.log(rt2.graphemeLength); // => 5
+    // const rt3 = new RichText({ text: '👨‍👩‍👧‍👧' });
+    // console.log(rt3.length); // => 25
+    // console.log(rt3.graphemeLength); // => 1
+
+    // console.log(markdown);
 };
 
 // const bleet =
